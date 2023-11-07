e2a6fe5ca37ebbc9989b26edd6c42ad6fbfbe495
maseian <149993928+maseian@users.noreply.github.com>
2023-11-06 18:20:04 -0600

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-fetch("https://api.hypixel.net/key?key=f5fd486c-8127-4f38-9204-a3a9d02ca7b6")
-    .then(result => result.json())
-    .then(({ record }) => {
-        // Log the owner's player UUID
-        console.log(record.ownerUuid)
-        print(record)
-    })
